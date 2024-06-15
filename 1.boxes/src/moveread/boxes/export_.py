b72e61d6408b9bb418396576e08bb92179f815de
m4rs <marcel@marcelclaramunt.cat>
2024-06-15 20:56:45 +0200

    auto-contours += paddings

--- 1.boxes/src/moveread/boxes/export_.py
+++ 1.boxes/src/moveread/boxes/export_.py
@@ -1,6 +1,6 @@
 from cv2 import Mat
 from .annotations import ExportableAnnotations
-from robust_extraction import extract_contours
+import robust_extraction as re
 from scoresheet_models import models
 from .model_extract import extract_grid, Pads
 
@@ -9,4 +9,5 @@ def export(img: Mat, ann: ExportableAnnotations, pads: Pads | None = None) -> li
   if ann.tag == 'grid':
     return extract_grid(img=img, coords=ann.grid_coords, model=models[ann.model], pads=pads)
   else:
-    return extract_contours(img=img, contours=ann.box_contours)
+    re_pads = re.Pads(**pads) if pads else None
+    return re.extract_contours(img=img, contours=ann.box_contours, pads=re_pads)
